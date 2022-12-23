7c25c6b1df43348de85eede8c997eb02234329c5
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-12-23 23:04:04 +0100

    update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="7.4"
 
 # dependencies used by the app
 pkg_dependencies="sqlite3 php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl"
